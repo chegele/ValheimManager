60d677156a610606eb6276418836a9480ac347bf
Chegele <schegele@hotmail.com>
2021-03-16 01:34:46 -0400

    Defined SystemProcess for returning process lookups

--- src/types/typedef.js
+++ src/types/typedef.js
@@ -1,3 +1,8 @@
 
 /** @typedef {import('../../index')}    Manager */
 /** @typedef {import('./config')}       Configuration */
+
+/**@typedef {Object} SystemProcess
+ * @property {String} pid
+ * @property {String} name                                        
+ */                     
